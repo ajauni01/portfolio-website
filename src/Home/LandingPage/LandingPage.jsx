59ab1d2fd14bcc4c9f8286e7125e177cbbdbbd69
AJHARUL ISLAM AUNIK <aunik@AJHARULs-Air.home>
2023-11-29 20:20:45 -0600

    add transition to the titles

--- src/Home/LandingPage/LandingPage.jsx
+++ src/Home/LandingPage/LandingPage.jsx
@@ -1,50 +1,77 @@
 import useMediaQuery from "../../hooks/useMediaQuery";
-import profileImg from "../../assets/profile-img-3.png"
+import profileImg from "../../assets/profile-img-3.png";
 import { motion } from "framer-motion";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-import { BsDownload } from 'react-icons/bs';
+import { BsDownload } from "react-icons/bs";
 import { Link } from "react-scroll";
 import SocialMediaIcons from "../../Components/SocialMediaIcons";
-
+import { useEffect, useState } from "react";
+import React from "react";
+import TextTransition, { presets } from "react-text-transition";
 
 const LandingPage = ({ setSelectedPage }) => {
-  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
 
-  //  function to handle resume download
-  // const handleResumeDownload = (event) => {
-  //   event.preventDefault();
-  //   const link = document.createElement('a');
-  //   link.href = 'https://drive.google.com/file/d/1F8hn5dysuc9jfU_apgpo6GE_ZQv7TFhN/view?usp=sharing';
-  //   link.setAttribute('download', 'Resume of Ajharul Islam Aunik.pdf');
-  //   link.target = '_blank';
-  //   link.rel = 'noopener noreferrer';
-  //   document.body.appendChild(link);
-  //   link.click();
-  //   document.body.removeChild(link);
-  // };
+  // titles array
+  const titles = [
+    "Web Developer",
+    "Full Stack Developer",
+    "Tech Enthusiast",
+    "Code Lover",
+    "Frontend Developer",
+    "Backend Developer",
+    "JavaScript Developer",
+    "Interface Designer",
+    "UI/UX Designer",
+    "React Developer",
+    "Node Developer",
+    "Innovation Enthusiast",
+  ];
+
+  // current title
+  const [index, setIndex] = React.useState(0);
+  // setInterval function to change the title every 3 seconds
+  React.useEffect(() => {
+    const intervalId = setInterval(
+      () => setIndex((index) => index + 1),
+      3000 // every 3 seconds
+    );
+    return () => clearTimeout(intervalId);
+  }, []);
 
   const handleResumeDownload = () => {
-    const link = document.createElement('a');
-    link.href = 'https://drive.google.com/uc?export=download&id=1F8hn5dysuc9jfU_apgpo6GE_ZQv7TFhN';
+    const link = document.createElement("a");
+    link.href =
+      "https://drive.google.com/uc?export=download&id=1F8hn5dysuc9jfU_apgpo6GE_ZQv7TFhN";
     // link.href = '/src/assets/resume/Resume of Ajharul Islam Aunik.pdf';
-    link.setAttribute('download', 'Resume of Ajharul Islam Aunik.pdf');
+    link.setAttribute("download", "Resume of Ajharul Islam Aunik.pdf");
     // link.target = '_blank';
     // link.rel = 'noopener noreferrer';
     link.click();
   };
 
   return (
-    <section id="home" className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10">
-
+    <section
+      id="home"
+      className="md:flex md:justify-between md:items-center md:h-full gap-16 py-10"
+    >
       {/* IMAGE SECTION */}
       <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-40">
         {isAboveMediumScreens ? (
           <div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[600px] before:w-full before:max-w-[500px] before:h-full before:border-2 before:border-blue before:z-[-1]">
-
-            <img className="hover:filter hover:saturate-150 transition duration-500  w-full max-w-[400px] md:max-w-[600px] rounded-t-[400px] bg-opacity-5" src={profileImg} alt="profile" />
-          </div>)
-
-          : (<img className="hover:filter hover:saturate-150 transition duration-500  w-full max-w-[400px] md:max-w-[600px] rounded-t-[400px] bg-opacity-5" src={profileImg} alt="profile" />)}
+            <img
+              className="hover:filter hover:saturate-150 transition duration-500  w-full max-w-[400px] md:max-w-[600px] rounded-t-[400px] bg-opacity-5"
+              src={profileImg}
+              alt="profile"
+            />
+          </div>
+        ) : (
+          <img
+            className="hover:filter hover:saturate-150 transition duration-500  w-full max-w-[400px] md:max-w-[600px] rounded-t-[400px] bg-opacity-5"
+            src={profileImg}
+            alt="profile"
+          />
+        )}
       </div>
 
       {/* MAIN SECTION */}
@@ -56,16 +83,25 @@ const LandingPage = ({ setSelectedPage }) => {
           transition={{ duration: 0.5 }}
           variants={{
             hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 50 }
+            visible: { opacity: 1, x: 50 },
           }}
         >
           <p className="text-5xl font-playfair z-10 text-start">
             Ajharul{" "}
-            <span className="xs:relative xs:text-white xs:font-semibold z-20 before:content-brush before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]"> Islam Aunik</span>
+            <span className="xs:relative xs:text-white xs:font-semibold z-20 before:content-brush before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]">
+              {" "}
+              Islam Aunik
+            </span>
           </p>
 
-          {/* TEXT */}
-          <p className="mt-10 mb-7 font-playfair text-lg text-start text-lg">Welcome to my World of Web Development: Exploring the Boundless Possibilities of the MERN Stack. edit it</p>
+          {/* Developer titles with text transition */}
+          <p className="mt-12 mb-10 text-start">
+            <TextTransition springConfig={presets.wobbly}>
+              <span className="text-5xl text-pink-500">
+                {titles[index % titles.length]}
+              </span>
+            </TextTransition>
+          </p>
         </motion.div>
 
         {/* CALL TO ACTIONS */}
@@ -77,10 +113,15 @@ const LandingPage = ({ setSelectedPage }) => {
           transition={{ delay: 0.2, duration: 0.5 }}
           variants={{
             hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 50 }
-          }}>
+            visible: { opacity: 1, x: 50 },
+          }}
+        >
           {/* CONTACT */}
-          <AnchorLink className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold hover:bg-blue hover:text-white transition duration-500" onClick={() => setSelectedPage("contact")} href="#contact">
+          <AnchorLink
+            className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold hover:bg-blue hover:text-white transition duration-500"
+            onClick={() => setSelectedPage("contact")}
+            href="#contact"
+          >
             Contact Me
           </AnchorLink>
 
@@ -97,26 +138,25 @@ const LandingPage = ({ setSelectedPage }) => {
               Resume <BsDownload className="ms-2 text-xl" />
             </div>
           </Link>
-
         </motion.div>
 
         {/* SOCIAL MEDIA ICONS */}
-        <motion.div className="flex justify-start"
+        <motion.div
+          className="flex justify-start"
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.5 }}
           transition={{ delay: 0.4, duration: 0.5 }}
           variants={{
             hidden: { opacity: 0, x: -50 },
-            visible: { opacity: 1, x: 50 }
-          }}>
+            visible: { opacity: 1, x: 50 },
+          }}
+        >
           <SocialMediaIcons />
         </motion.div>
-
       </div>
     </section>
   );
 };
 
 export default LandingPage;
-
